680850fae7120a13e1e9138cd6c813b3b2e9b676
HamaHs <lusuy1995@gmail.com>
2024-01-27 16:30:25 +0100

    some changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import "./App.css";
+import "./App.scss";
+import Title from "./components/Title/Title";
+import Carousel from "./components/Carousel";
 
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <Title>Our Reviews</Title>
+
+         <Carousel />
+      </div>
+   );
 }
 
 export default App;
