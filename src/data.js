7dce3889292b6fcf4ad1c99e782722c1cf22305b
HamaHs <lusuy1995@gmail.com>
2024-01-27 16:29:55 +0100

    added BD of rewiews

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const cardsData = [
+   {
+      img: "",
+      name: "Susan Smith",
+      job: "web developer",
+      about: "Lorem, ipsum pisicing elit. Repudiandae, dolorem saepe impedit sequi nobis exercitationem fuga maxime omnis unde eaque repellendus, dignissimos ad. Quibusdam consequatur illum nam distinctio corporis odit!",
+   },
+   {
+      img: "",
+      name: "Jackson Nalivo",
+      job: "system administrator",
+      about: "Lorem, ipsum dolor simet consectetur adipisicing elit. Repudiandae, dolorem saepe impedit sequi nobis exercitationem fuga maxime omnis unde eaque repellendus, dignissimos ad. Quibusdam consequatur illum nam poris odit!",
+   },
+   {
+      img: "",
+      name: "Diksy Donson",
+      job: "firewoman",
+      about: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, dolorem saepe impedit tionem fuga maxime omnis unde eaque repellendus, dignissimos ad. Quibusdam consequatur illum nam distinctio corporis odit!",
+   },
+   {
+      img: "",
+      name: "Dayana Mitchel",
+      job: "social worker",
+      about: "Lorem, ipsum dolor simet consectetur adipisicing elit. Repudiandae, dolorem saepe impedit sequi nobis exercitationem fuga maxime omnis unde eaque repellendus, dignissimos ad. Quibusdam consequatur illum nam poris odit!",
+   },
+   {
+      img: "",
+      name: "Nelson Trate",
+      job: "police officer",
+      about: "Lorem, ipsum dolor simet consectetur adipisicing elit. Repudiandae, dolorem saepe impedit sequi nobis exercitationem fuga maxime omnis unde eaque repellendus, dignissimos ad. Quibusdam consequatur illum nam poris odit!",
+   },
+];
+
+export default cardsData;
